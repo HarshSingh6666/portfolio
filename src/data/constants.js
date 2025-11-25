b7e3fdfd02ac03be548f568773117acdcc48eaf8
Harsh Kumar Singh <harshkumarsingh23272598@gmail.com>
2025-11-25 09:49:12 +0530

    Change resume link to Google Drive
    Updated resume link to point to a Google Drive file.

--- src/data/constants.js
+++ src/data/constants.js
@@ -9,7 +9,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/HarshSingh6666",
   resume:
-    " ",
+    "https://drive.google.com/file/d/1XVFMaoijyQ26o8jGO8IDZG4Kr5ne-fN7/view?usp=sharing",
   linkedin:"https://www.linkedin.com/in/harshkumarsingh666?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BFMDVrc%2FsSpqT9S1jvXRVtw%3D%3D",
   twitter:"/",
   insta:"https://www.instagram.com/hk_singh_666/",
